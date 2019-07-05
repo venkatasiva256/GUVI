40bbdb17bd4b91968dfeba23f957a70109021063
venkatasiva256 <52446523+venkatasiva256@users.noreply.github.com>
2019-07-05 10:03:49 +0530

    Update vowel_or_conso.py

--- vowel_or_conso.py
+++ vowel_or_conso.py
@@ -1,9 +1,9 @@
-let_list=['a','e','i','o','u']
-let_list1=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+vowels=['a','e','i','o','u']
+consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 letter=str(input())
-if (letter in let_list):
+if (letter in vowels):
     print("Vowel")
-elif(letter in let_list1):
+elif(letter in consonants):
   print("Consonant")
 else:
  	print("invalid")
