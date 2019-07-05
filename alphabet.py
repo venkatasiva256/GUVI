ead519d480cd81cd542b4455375b6b39b1dfbde5
venkatasiva256 <52446523+venkatasiva256@users.noreply.github.com>
2019-07-05 10:11:07 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+inp=str(input())
+if (inp in alphabets):
+    print("Alphabet")
+else:
+ 	print("No")
