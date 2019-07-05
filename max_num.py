e7132b5be813506da2aa11beb57fa78acf92e8ac
venkatasiva256 <52446523+venkatasiva256@users.noreply.github.com>
2019-07-05 11:42:30 +0530

    Update max_num.py

--- max_num.py
+++ max_num.py
@@ -1,3 +1,13 @@
-num=input().split()
-a=max(num)
-print(a)
+def max_num(n1,n2,n3):
+	if(n1>=n2)and (n1>=n3):
+		max=n1
+	elif(n2>=n1)and(n2>=n3):
+		max=n2
+	elif(n3>=n1)and(n3>=n2): 
+		max=n3
+	return max
+n1=int(input())
+n2=int(input())
+n3=int(input())
+print(max_num(n1,n2,n3))
+
