264a846871654231d37017113827a4c80d585de6
venkatasiva256 <52446523+venkatasiva256@users.noreply.github.com>
2019-07-11 09:49:34 +0530

    Create roman_no.py

---
+++ roman_no.py
@@ -0,0 +1,7 @@
+list1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+list2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+w=str(input())
+for i in range(20):
+  if(w==list1[i]):
+    print(list2[i])
+    break
