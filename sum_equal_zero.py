8d3801826a95ac6a446063072380d4a327627d63
venkatasiva256 <52446523+venkatasiva256@users.noreply.github.com>
2019-07-17 11:56:02 +0530

    Update sum_equal_zero.py

--- sum_equal_zero.py
+++ sum_equal_zero.py
@@ -1,8 +1,10 @@
-a=int(input())
-b=map(int,input().split())
-b = list(b)
-for i in range(a):
-  for j in range(i+1,a):
-    if(b[i]+b[j]==0):
-      print(b[i],b[j])
-      break
+m=int(input())
+n=list(map(int,input().split()))
+c=max(n)
+d,e=0,0
+for i in range(0,len(n)-1):
+  for j in range(i+1,len(n)):
+    if abs(n[i]+n[j])<c:
+      d,e=n[i],n[j]
+      c=abs(d+e)
+print(d,e)
